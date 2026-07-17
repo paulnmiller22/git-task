b46336a705f5aa1c2cc996a6474db5cd8f0c3777
paulnmiller <paulnmiller22@gmail.com>
2026-07-17 11:32:08 -0500

    modified files and removed older files

--- hello_world.py
+++
@@ -1,3 +0,0 @@
-# Print "Hello World!" for this program to show new folder "git-task" now has a file in it.
-
-print("Git is awesome!") # Change file message from "Hello World!" to "Git is awesome!".git 
